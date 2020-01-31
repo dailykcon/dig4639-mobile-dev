93ad8adac62aadfc8620e1552a5bba126ecac5fd
Kevin Connolly <46018813+dailykcon@users.noreply.github.com>
2020-01-31 10:07:43 -0500

    Lab 1

---
+++ labs/1/index.js
@@ -0,0 +1,11 @@
+var Adder = require("./Adder.js");
+var adderInstance = new Adder();
+var input = {
+    a: 10,
+    b: 5,
+};
+
+var adderInstance = new Adder(input);
+
+
+console.log(adderInstance.render());
