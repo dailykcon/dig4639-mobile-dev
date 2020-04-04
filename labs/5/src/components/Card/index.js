d284b9e339911c04ac8dcbdd5059807db38e51cc
Kevin Connolly <46018813+dailykcon@users.noreply.github.com>
2020-04-04 15:32:21 -0400

    Lab 5 Complete

---
+++ labs/5/src/components/Card/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './index.css'
+
+class Card extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: '',
+      temperature: 0,
+      temperatureUnit: '',
+      detailedForecast: ''
+    };
+  }
+
+  render () {
+    return (
+      <div className="card">
+        <h1 className="title">{this.props.name}</h1>
+        <h1 className="temp">{this.props.temperature} {this.props.temperatureUnit}</h1>
+        <p>{this.props.detailedForecast}</p>
+      </div>
+    )
+  }
+}
+
+export default Card;
---
+++ labs/5/src/components/WeatherList/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Card from '../Card/index.js'
+
+class WeatherList extends React.Component {
+  
+  constructor (props) {
+    super(props)
+    this.state = {periods: []}
+  }
+
+  componentDidMount() {
+
+    fetch('https://api.weather.gov/gridpoints/MLB/25,69/forecast')
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          periods: result.properties.periods
+        })
+      })
+
+  }
+  
+  render () {
+    return (
+      <div>
+        {
+        this.state.periods.map((v) => {
+          return <Card
+            key = {v.number}
+            name = {v.name}
+            temperature = {v.temperature}
+            temperatureUnit = {v.temperatureUnit}
+            detailedForecast = {v.detailedForecast}
+          />
+          })
+        } 
+      </div>
+    );
+  }
+}
+
+export default WeatherList;
